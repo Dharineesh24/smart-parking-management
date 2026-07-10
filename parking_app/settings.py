3b5bd0d8eb30932c1240995777244709b7f56c9e
Dharineesh A <dharineeshmohan2424@gmail.com>
2026-07-10 12:14:51 +0530

    Render settings

--- parking_app/settings.py
+++ parking_app/settings.py
@@ -116,5 +116,6 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
